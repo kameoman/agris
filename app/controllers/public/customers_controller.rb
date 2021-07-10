d0adba564b96fda450a8f1f34e3ae33b5ea6a1dc
kameoman <spiderman.935963@gmail.com>
2021-07-10 06:25:15 +0000

    add

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -3,6 +3,7 @@ class Public::CustomersController < ApplicationController
 
   def index
     @customer = current_customer
+    @customers = Customer.all
         # クエリストリングがあればTimeオブジェクトに変換、ない場合は現在の時刻を取得
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     # 取得した時刻が含まれる月の範囲のデータを取得
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_063041) do
+ActiveRecord::Schema.define(version: 2021_07_10_055144) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_07_07_063041) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "customer_id"
   end
 
 end
