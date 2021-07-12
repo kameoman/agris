749956adac7bb6971613b64b0be41e1b24aa3403
kameoman <spiderman.935963@gmail.com>
2021-07-12 12:18:42 +0000

    name

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,21 +5,23 @@ class Admin::CustomersController < ApplicationController
     # 取得した時刻が含まれる月の範囲のデータを取得
     @items = Item.where(date: @month.all_month).order('date ASC')
     @customer = Customer.all
+    # binding pry
+    @customer_items = Customer.joins(:items)
+    # SELECT `customers`.* FROM `customers` INNER JOIN `items` ON `customer_id` = `customers`.`id`
   end
-  
+
   def profile
     @customers = Customer.all
   end
-  
+
   def show
-    # binding pry
     @customer = Customer.find(params[:id])
         # クエリストリングがあればTimeオブジェクトに変換、ない場合は現在の時刻を取得
     @month = params[:month] ? Date.parse(params[:month]) : Time.zone.today
     # 取得した時刻が含まれる月の範囲のデータを取得
     @items = @customer.items.where(date: @month.all_month).order('date ASC')
   end
-  
 
-  
+
+
 end
