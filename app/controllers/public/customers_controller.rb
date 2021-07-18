03a39cd9a21398d33cf14f2db441f9091a4a707d
kameoman <spiderman.935963@gmail.com>
2021-07-18 03:32:41 +0000

    about-fix

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -33,6 +33,7 @@ class Public::CustomersController < ApplicationController
 
   def edit
     @customer = Customer.find(params[:id])
+    @customers = Customer.find(params[:id])
   end
 
   def update
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,10 +7,6 @@ class Customer < ApplicationRecord
   belongs_to :admin, optional: true
   has_many :items
 
-  # addressを登録した際にgeocoderが緯度、経度のカラムにも自動的に値を入れてくれる
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
-
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |customer|
       customer.password = SecureRandom.urlsafe_base64
@@ -18,4 +14,10 @@ class Customer < ApplicationRecord
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
   end
+
+
+  # addressを登録した際にgeocoderが緯度、経度のカラムにも自動的に値を入れてくれる
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
 end
