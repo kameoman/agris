105c5e51607afe958394e480bf41faa7357fd319
kameoman <spiderman.935963@gmail.com>
2021-07-28 23:16:00 +0000

    815

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,6 +33,18 @@
     });
   });
 
+  $(function() {
+    document.getElementById("navbtn").onclick = function(){
+      document.querySelector('html').classList.toggle('open');
+    }
+    document.getElementById("nav-link").onclick = function(){
+      document.querySelector('html').classList.remove('open');
+    }
+  });
+
+
+
+
 
 
 
--- app/assets/javascripts/slide.js
+++ app/assets/javascripts/slide.js
@@ -44,14 +44,3 @@ document.addEventListener("turbolinks:load", function() {
 
 });
 
-document.addEventListener("turbolinks:load", function() {
-
-  document.getElementById("navbtn").onclick = function(){
-    document.querySelector('html').classList.toggle('open');
-  }
-  document.getElementById("nav-link").onclick = function(){
-    document.querySelector('html').classList.remove('open');
-  }
-
-
-});
